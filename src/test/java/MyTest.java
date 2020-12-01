7aed048f2a2ff696084ad921f5f43e91623d653f
Michele Biancucci <michelebiancucci@gmail.com>
2020-12-01 16:14:55 +0100

    Update MyTest.java
    added three kisses

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -6,9 +6,12 @@ public class MyTest {
     public void test_method_1() {
         Demo d = new Demo();
         d.DoSomething(true);
+        for (int i = 0; i < 3; i++) {  
+            d.DoSomethingBeautiful();
+        }       
     }
 
     @Test
-    public void test_method_2() {
+    public void test_method_2() {        
     }
 }
